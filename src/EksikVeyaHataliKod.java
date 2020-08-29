58f4889e6aa49b010dd7e7527ff63e9213e1f867
maolukpinar <maolukpinar@gmail.com>
2020-08-29 23:30:51 +0200

    ikinci yükleme yedek'e

---
+++ src/EksikVeyaHataliKod.java
@@ -0,0 +1,6 @@
+public class EksikVeyaHataliKod {
+    public static void main(String[] args) {
+        System.out.println("Tamamlanmamış kod");
+        System.out.println("hadibakalim");
+    }
+}
